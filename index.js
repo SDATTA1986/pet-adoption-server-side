4956233626463aba3be75dc6472bdc4494fb6111
SOURAV DATTA <sourav@SOURAVs-MacBook-Air.local>
2023-12-04 22:13:52 +0530

    admin verifying API created

--- index.js
+++ index.js
@@ -233,6 +233,21 @@ async function run() {
         res.send(result);
       })
 
+      app.get('/users/admin/:email', verifyToken, async (req, res) => {
+        const email = req.params.email;
+  
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: 'forbidden access' })
+        }
+        const query = { email: email };
+        const user = await userCollection2.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user?.role === true;
+        }
+        res.send({ admin });
+      })
+
     app.get('/payments',async(req,res)=>{
         const email=req.query.email;
         const query={email:email};
