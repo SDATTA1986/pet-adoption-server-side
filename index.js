e2406c284b540b7f16013aca1aaf6e011b92eef9
SOURAV DATTA <sourav@SOURAVs-MacBook-Air.local>
2023-12-04 12:19:23 +0530

    create campaign post api created

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
+const jwt=require('jsonwebtoken');
+const cloudinary=require('cloudinary').v2;
 require('dotenv').config()
 const stripe=require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port=process.env.PORT || 5000;
-
+const fileUpload=require('express-fileupload');
+app.use(fileUpload({
+    useTempFiles:true
+}))
 
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+ cloudinary.config({
+    cloud_name: `${process.env.CLOUD_NAME}`,
+    api_key: `${process.env.API_KEY}`,
+    api_secret: `${process.env.API_SECRET}`,
+  });
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4qnhmwc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +46,38 @@ async function run() {
     const paymentCollection=client.db("PetAdoption").collection("Payment");
     const userCollection2=client.db("PetAdoption").collection("Users");
 
+
+    app.post('/',(req,res,next)=>{
+        console.log(req.body);
+        const file=req.files.photo;
+        cloudinary.uploader.uploadFile.tempFilePath,(err,result)=>{
+            console.log(result);
+        }
+    })
+
+    app.post('/jwt',async(req,res)=>{
+        const user=req.body;
+        const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:'1h'
+        });
+        res.send({token});
+    })
+
+    const verifyToken=(req,res,next)=>{
+        console.log('inside verify token',req.headers.authorization);
+        if(!req.headers.authorization){
+            return res.status(401).send({message:'forbidden access'});
+        }
+        const token=req.headers.authorization.split(' ')[1];
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+            if(err){
+                return res.status(401).send({message:'Forbidden access'})
+            }
+            req.decoded=decoded;
+            next();
+        })
+    }
+
     app.post('/users',async(req,res)=>{
         const user=req.body;
         const query={email:user.email};
@@ -47,6 +93,36 @@ async function run() {
         const result=await petCollection.find().toArray();
         res.send(result);
     })
+
+    app.get('/Pet/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)}
+        const result=await petCollection.find(query).toArray();
+        res.send(result);
+    })
+
+    app.put("/Pet/:id",async(req,res)=>{
+        const id=req.params.id;
+        const filter={_id:new ObjectId(id)}
+        const options={upsert:true};
+        const updatedProduct=req.body;
+        const product={
+          $set:{
+            PetImage:updatedProduct.PetImage,
+            PetName:updatedProduct.PetName,
+            PetLocation:updatedProduct.PetLocation,
+            PetAge:updatedProduct.PetAge,
+            PetCategory:updatedProduct.PetCategory,
+            ShortDescription:updatedProduct.ShortDescription,
+            LongDescription:updatedProduct.LongDescription,
+            DateOfUpdation:updatedProduct.DateOfUpdation,
+            TimeOfUpdation:updatedProduct.TimeOfUpdation
+          }
+        }
+        const result=await petCollection.updateOne(filter,product,options);
+        res.send(result);
+      })
+
     app.get('/category',async(req,res)=>{
         const result=await categoryCollection.find().toArray();
         res.send(result);
@@ -70,6 +146,22 @@ async function run() {
 
     })
 
+    app.post("/addPet", async (req, res) => {
+        const user = req.body;
+        console.log(user);
+        const result = await petCollection.insertOne(user);
+        console.log(result);
+        res.send(result);
+      });
+
+      app.post("/createCampaign", async (req, res) => {
+        const user = req.body;
+        console.log(user);
+        const result = await campaignCollection.insertOne(user);
+        console.log(result);
+        res.send(result);
+      }); 
+
     app.post('/payments',async(req,res)=>{
         const payment=req.body;
         const paymentResult=await paymentCollection.insertOne(payment);
@@ -82,6 +174,30 @@ async function run() {
     app.get('/payment/:email',async(req,res)=>{
         const query={email:req.params.email}
         const result=await paymentCollection.find(query).toArray();
+        res.send(result);
+    })
+    app.get('/Pets',async(req,res)=>{
+        const query={email:req.query.email}
+        const result=await petCollection.find(query).toArray();
+        res.send(result);
+    })
+
+    app.delete('/Pets/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)}
+        const result=await petCollection.deleteOne(query);
+        res.send(result);
+    })
+
+    app.patch('/Pets/adopted/:id',async(req,res)=>{
+        const id=req.params.id;
+        const filter={_id: new ObjectId(id)};
+        const updatedDoc={
+            $set:{
+                Adopted: Boolean(1)
+            }
+        }
+        const result=await petCollection.updateOne(filter,updatedDoc);
         res.send(result);
     })
 
